9ba6a988cad6f9a8d0a8a99d924fda97f6548185
denverma7 <denverma7@gmail.com>
2026-06-04 20:58:13 +0500

    store and carts functionality

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Variation
 
 # Register your models here.
 
@@ -7,5 +7,11 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock',  'category', 'modified_date', 'is_available') # to show the product name, price, stock, availability, category, created date and modified date fields in admin panel
     prepopulated_fields = {'slug':('product_name',)} # to automatically populate the slug field based on the product name field in admin panel}
 
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value')
+
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
